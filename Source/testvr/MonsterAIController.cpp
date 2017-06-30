f64f8568491beaa998b3d2f04a9a22be90633dc7
andchelsea <chelsea@virtro.ca>
2017-06-29 22:11:04 -0700

    AI start

---
+++ Source/testvr/MonsterAIController.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MonsterAIController.h"
+
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+AMonsterAIController::AMonsterAIController()
+	//:mBlackboard(CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackBoard")))
+	//, mBehaviortree(CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorTree")))
+{
+	//AddInstanceComponent(mBlackboard);
+	//AddOwnedComponent(mBehaviortree);
+
+	//bWantsPlayerState = true;
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMonsterAIController::Possess(APawn * InPawn)
+{
+}
+
+void AMonsterAIController::BeginInactiveState()
+{
+}
+
+
