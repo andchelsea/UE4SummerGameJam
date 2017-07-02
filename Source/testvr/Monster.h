cd6a87824a4f8ec7fe6c065a7c5c6c5999b7747b
andchelsea <chelsea@virtro.ca>
2017-07-01 17:01:01 -0700

    chelseas stuff

--- Source/testvr/Monster.h
+++ Source/testvr/Monster.h
@@ -24,12 +24,13 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	//UCharacterMovementComponent* Movement;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 
 };
--- Source/testvr/MonsterAIController.cpp
+++ Source/testvr/MonsterAIController.cpp
@@ -6,7 +6,8 @@
 #include "BehaviorTree/Blackboard/BlackboardKeyType_Vector.h"
 //#include "BehaviorTree/Blackboard/BlackboardKeyType_Enum.h"
 #include "Monster.h"
-#include "VRPlayer.h"
+#include "Item.h"
+#include "time.h"
 
 AMonsterAIController::AMonsterAIController()
 	:mBlackboard(CreateOptionalDefaultSubobject<UBlackboardComponent>(TEXT("BlackBoard")))
@@ -17,6 +18,8 @@ AMonsterAIController::AMonsterAIController()
 
 	//bWantsPlayerState = true;
 	PrimaryActorTick.bCanEverTick = true;
+
+	srand(time(NULL));
 }
 
 void AMonsterAIController::Possess(APawn * InPawn)
@@ -37,14 +40,37 @@ void AMonsterAIController::Possess(APawn * InPawn)
 		//mBlackboard->SetValue<UBlackboardKeyType_Enum>(StateEnum, AIstate::Entering);
 		mBehaviortree->StartTree(*mOwner->BehaviorTree);
 	}
+	
+	mDesiredWeapon = NewObject<AItem>();
+
+	switch (rand()%3)//set weapon type
+	{
+		case 0:
+			mDesiredWeapon->SetWeaponType(WeaponType::kSword);
+			break;
+		case 1:
+			mDesiredWeapon->SetWeaponType(WeaponType::kAxe);
+			break;
+		case 2:
+			mDesiredWeapon->SetWeaponType(WeaponType::kLance);
+			break;
+	}
+
+	switch (rand() % 3)//set weapon material
+	{
+	case 0:
+		mDesiredWeapon->SetMaterialType(MaterialType::kCopper);
+		break;
+	case 1:
+		mDesiredWeapon->SetMaterialType(MaterialType::kGold);
+		break;
+	case 2:
+		mDesiredWeapon->SetMaterialType(MaterialType::kIron);
+		break;
+	}
 }
 
 void AMonsterAIController::BeginInactiveState()
 {
 }
-
-void AMonsterAIController::AngerMonster()
-{
-}
-
 
